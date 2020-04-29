e6f8b2f2e6ad36682b275371f3352129e69372ac
Uditha Karunaratne <uditha.karunaratne@infor.com>
2020-04-30 04:32:52 +1000

    Add Ido/Vertical Ido/Smex to the Cofig, Organize the config

--- config.el
+++ config.el
@@ -1,32 +1,3 @@
-;; Update Default Settings
-(setq test "test" )
-(setq default-directory "~/")
-(setq ring-bell-function 'ignore)
-(setq inhibit-startup-message t)
-(prefer-coding-system 'utf-8)
-(setq-default buffer-file-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8)
-(set-language-environment 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(setq visible-bell t)
-(setq make-backup-file nil)
-(setq auto-save-default nil)
-(global-display-line-numbers-mode)
-
-(when window-system (global-hl-line-mode t))
-(when window-system (global-prettify-symbols-mode t))
-
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-;; change all prompts to y or n
-(fset 'yes-or-no-p 'y-or-n-p)
-
 (defconst sys/win32p
   (eq system-type 'windows-nt)
   "Are we running on a WinTel system?")
@@ -39,31 +10,94 @@
   (eq system-type 'darwin)
   "Are we running on a Mac system?")
 
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-message t)
+
+(setq default-directory "~/")
+
+;;Fuck that bell
+(setq ring-bell-function 'ignore)
+
+(prefer-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+
+(setq visible-bell t)
+(setq make-backup-file nil)
+(setq auto-save-default nil)
+
+(when window-system (global-hl-line-mode t))
+(when window-system (global-prettify-symbols-mode t))
+
+(global-display-line-numbers-mode)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(substitute-key-definition 'kill-buffer
+			   'kill-buffer-and-window
+			   global-map)
+
 (defvar my-term-shell "/bin/bash")
 (when sys/linuxp (setq my-term-shell "/bin/bash"))
 (when sys/win32p (setq my-term-shell "C:\\Program Files\\Git\\bin\\bash.exe"))
 (when sys/macp (setq my-term-shell "/bin/bash"))
 
 (defun run-bash ()
-      (interactive)
-      (let ((shell-file-name my-term-shell))
-	(shell "*bash*")))
+  (interactive)
+  (let ((shell-file-name my-term-shell))
+    (shell "*bash*")))
 
 (defun run-cmdexe ()
-      (interactive)
-      (let ((shell-file-name "cmd.exe"))
-	    (shell "*cmd.exe*")))
+  (interactive)
+  (let ((shell-file-name "cmd.exe"))
+    (shell "*cmd.exe*")))
 
 (defun my-windows-shell-setup () (interactive)
-     ;; The variable `git-shell-path' contains the path to the `Git\bin'
-     ;; file on my system. I install this in      
-     (setq explicit-shell-file-name my-term-shell)
-     (setq explicit-bash.exe-args '("--login" "-i"))
-     (message "Windows preferences set."))
+       ;; The variable `git-shell-path' contains the path to the `Git\bin'
+       ;; file on my system. I install this in      
+       (setq explicit-shell-file-name my-term-shell)
+       (setq explicit-bash.exe-args '("--login" "-i"))
+       (message "Windows preferences set."))
 
 (if sys/win32p (my-windows-shell-setup))
 
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+;; Enable line wrap in Org mode
+(add-hook 'org-mode-hook '(lambda () (visual-line-mode 1)))
+(setq org-agenda-files (append
+			(file-expand-wildcards  "~/org-notes/*.org")))
+
+;; Key Binding
+(global-set-key (kbd "C-c I") #'my-find-user-init-file)
+
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(use-package ido-vertical-mode
+	      :ensure t
+	      :init
+	      (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind ("M-x" . smex ))
+
+;; (global-set-key (kbd "C-x b") 'ido-switch-buffer) - This is the default
 
 (use-package which-key
   :ensure t
@@ -73,25 +107,9 @@
   :ensure t
   :init (beacon-mode 1))
 
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-
-;; Util Functions
 (defun my-open-init-file ()
   "Edit the `user-init-file', in another window."
   (interactive)
   (find-file-other-window user-init-file))
 
-
-;; Key Binding
-(global-set-key (kbd "C-c I") #'my-find-user-init-file)
-
-
-;;; Org Mode Stuff
-;; Enable line wrap in Org mode
-(add-hook 'org-mode-hook '(lambda () (visual-line-mode 1)))
-(setq org-agenda-files (append
-			(file-expand-wildcards  "~/org-notes/*.org")))
+(global-set-key (kbd "C-x C-b") 'ibuffer)
--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Emacs configuration of Uditha Karunaratn
 
-(when (version< emacs-version "26.3")
+(when (version< emacs-version "26.0")
   (warn "This configuration needs Emacs trunk, but this is %s!" emacs-version))
 
 ;; Increase the garbage collection threshold to 500 MB to ease startup
@@ -35,14 +35,10 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update))))
+    (smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "outline" :family "Source Code Pro")))))
-
-
-
-
