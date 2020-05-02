a9a12d4b92cf3d35ab2f3c39c28164d1e168089c
Uditha Karunaratne <uditha.karunaratne@infor.com>
2020-05-02 18:28:28 +1000

    More Customizations

--- config.el
+++ config.el
@@ -10,6 +10,21 @@
   (eq system-type 'darwin)
   "Are we running on a Mac system?")
 
+(line-number-mode 1)
+(column-number-mode 1)
+
+(global-subword-mode 1)
+
+(setq electric-pair-pairs '(
+			    (?\( . ?\))
+			    (?\[ . ?\])
+			    (?\{ . ?\})
+			    ))
+(electric-pair-mode 1)
+
+(setq display-time-24hr-format t)
+(display-time-mode 1)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -40,10 +55,6 @@
 (global-display-line-numbers-mode)
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(substitute-key-definition 'kill-buffer
-			   'kill-buffer-and-window
-			   global-map)
-
 (defvar my-term-shell "/bin/bash")
 (when sys/linuxp (setq my-term-shell "/bin/bash"))
 (when sys/win32p (setq my-term-shell "C:\\Program Files\\Git\\bin\\bash.exe"))
@@ -67,6 +78,10 @@
        (message "Windows preferences set."))
 
 (if sys/win32p (my-windows-shell-setup))
+
+(setq org-src-window-setup 'current-window)
+(add-to-list 'org-structure-template-alist 
+'("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
 
 (use-package org-bullets
   :ensure t
@@ -103,13 +118,119 @@
   :ensure t
   :init (which-key-mode))
 
+(use-package hungry-delete
+  :ensure t
+  :config (global-hungry-delete-mode t))
+
 (use-package beacon
   :ensure t
   :init (beacon-mode 1))
+
+(defun kill-whole-word ()
+  (interactive)
+  (backward-word)
+  (kill-word 1))
+
+(global-set-key (kbd "C-c w w") 'kill-whole-word)
 
 (defun my-open-init-file ()
   "Edit the `user-init-file', in another window."
   (interactive)
   (find-file-other-window user-init-file))
 
+(defun copy-whole-line()
+  (interactive)
+  (save-excursion (kill-new
+		   (buffer-substring (point-at-bol)
+				     (point-at-eol)))))
+
+(global-set-key (kbd "C-c w l") 'copy-whole-line)
+
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
+(global-set-key (kbd "C-c e") 'config-visit)
+
+(defun config-reload ()
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(global-set-key (kbd "C-c r") 'config-reload)
+
+(substitute-key-definition 'kill-buffer
+			   'kill-buffer-and-window
+			   global-map)
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(defun kill-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+(global-set-key (kbd "C-c k k") 'kill-all-buffers)
+
+(use-package avy
+  :ensure t
+  :bind ("M-s" . avy-goto-char))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init (rainbow-delimiters-mode 1))
+
+(use-package switch-window
+  :ensure t
+  :config
+  (setq switch-window-input-style 'minibuffer)
+  (setq switch-window-increase 4)
+  (setq switch-window-threshold 2)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts '("a" "s" "d" "f" "h" "j" "k" "l"))
+  :bind ([remap other-window] . switch-window))
+
+(defun split-and-follow-horizontally()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+
+(defun split-and-follow-vertically()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents . 10)))
+  (setq dashboard-banner-logo-title "Hello World!"))
+
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package spaceline
+    :ensure t
+    :config
+    (require 'spaceline-config)
+    (setq powerline-default-separator (quote arrow))
+    (spaceline-spacemacs-theme))
+
+(use-package diminish
+  :ensure t
+  :init
+  (diminish 'hungry-delete-mode)
+  (diminish 'beacon-mode)
+  (diminish 'which-key-mode)
+  (diminish 'subword-mode))
+
+(use-package dmenu
+  :ensure t
+  :bind
+  ("C-c d" . dmenu))
+
+(use-package symon
+  :ensure t
+  :bind ("C-c p" . 'symon-mode))
