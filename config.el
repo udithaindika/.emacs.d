f0ba1dc063c52162529709c19956a4e9fdddc6d2
Uditha Karunaratne <uditha.karunaratne@infor.com>
2020-05-09 18:36:27 +1000

    Fix the GitIgnore

--- config.el
+++ config.el
@@ -10,6 +10,9 @@
   (eq system-type 'darwin)
   "Are we running on a Mac system?")
 
+(use-package use-package-ensure-system-package
+  :ensure t)
+
 (line-number-mode 1)
 (column-number-mode 1)
 
@@ -29,37 +32,41 @@
 (setq auto-window-vscroll nil)  
 
 (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (scroll-bar-mode -1)
-  (setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-message t)
 
-  (setq default-directory "~/")
+(setq default-directory "~/")
 
-  ;;Fuck that bell
-  (setq ring-bell-function 'ignore)
+;;Fuck that bell
+(setq ring-bell-function 'ignore)
 
-  (prefer-coding-system 'utf-8)
-  (setq-default buffer-file-coding-system 'utf-8-unix)
-  (set-terminal-coding-system 'utf-8)
-  (set-language-environment 'utf-8)
-  (set-keyboard-coding-system 'utf-8)
-  (prefer-coding-system 'utf-8)
-  (setq locale-coding-system 'utf-8)
-  (set-default-coding-systems 'utf-8)
-  (set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
 
-  (setq visible-bell t)
-  (setq make-backup-file nil)
-  (setq auto-save-default nil)
+(setq visible-bell t)
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+(setq create-lockfiles nil)  ; stop creating .# files
 
-  (when window-system (global-hl-line-mode t))
-  (when window-system (global-prettify-symbols-mode t))
 
-  (global-display-line-numbers-mode)
-  (fset 'yes-or-no-p 'y-or-n-p)
+(when window-system (global-hl-line-mode t))
+(when window-system (global-prettify-symbols-mode t))
 
-  (setq visible-cursor nil)
+(global-display-line-numbers-mode)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq visible-cursor nil)
 (blink-cursor-mode  `blink-cursor-blinks)
+
+(global-auto-revert-mode nil)
 
 (defvar my-term-shell "/bin/bash")
 (when sys/linuxp (setq my-term-shell "/bin/bash"))
@@ -149,10 +156,15 @@
 (defun copy-whole-line()
   (interactive)
   (save-excursion (kill-new
-		   (buffer-substring (point-at-bol)
-				     (point-at-eol)))))
+                   (buffer-substring (point-at-bol)
+                                     (point-at-eol)))))
 
 (global-set-key (kbd "C-c w l") 'copy-whole-line)
+
+;; If nothing is marked yanks whole line
+(use-package whole-line-or-region
+  :ensure t
+  :config (whole-line-or-region-global-mode 1))
 
 (defun config-visit ()
   (interactive)
@@ -278,52 +290,124 @@
   :ensure t
   :bind ("C-q" . 'er/expand-region))
 
-;; JavaScript mode
-    ;; Better highlighting for JS files (potential support for JSX too)
-    (use-package js2-mode
-      :ensure t
-      :interpreter ("node" . js2-mode)
-      :mode ("\\.m?jsx?\\'" . js2-mode)
-      :config (setq js2-basic-offset 2
-                    js2-indent-switch-body t
-                    js2-strict-missing-semi-warning nil
-                    js2-mode-show-strict-warnings nil))
-
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  ;; Better imenu
-  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
-  (use-package prettier-js
-    :after js2-mode
-    :init
-    (add-hook 'js2-mode-hook 'prettier-js-mode)
-    (add-hook 'web-mode-hook 'prettier-js-mode)
-    :config
-    (setq prettier-js-args '("--trailing-comma" "all"
-                             "--bracket-spacing" "false"
-                             "--print-width" "200")))
-
-(add-hook 'prog-mode-hook #'hs-minor-mode)
-(global-set-key (kbd "C-c <right>") 'hs-show-block)
-(global-set-key (kbd "C-c <left>") 'hs-hide-block)
-
-
-;; (add-hook 'js2-mode-hook
+(setq test 1)
+  ;; JavaScript mode
+  ;; Better highlighting for JS files (potential support for JSX too)
+  ;; (use-package js2-mode
+  ;;   :ensure t
+  ;;   :interpreter ("node" . js2-mode)
+  ;;   :mode ("\\.m?jsx?\\'" . js2-mode)
+  ;;   :config (setq js2-basic-offset 2
+  ;;                 js2-indent-switch-body t
+  ;;                 js2-strict-missing-semi-warning t
+  ;;                 js2-mode-show-strict-warnings nil))
+  ;;
+  ;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  ;; ;; Better imenu
+  ;; (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+  ;;
+  ;; (use-package prettier-js
+  ;;   :after js2-mode
+  ;;   :init
+  ;;   (add-hook 'js2-mode-hook 'prettier-js-mode)
+  ;;   (add-hook 'web-mode-hook 'prettier-js-mode)
+  ;;   :config
+  ;;   (setq prettier-js-args '("--trailing-comma" "all"
+  ;;                            "--bracket-spacing" "false"
+  ;;                            "--print-width" "200")))
+  ;;
+  ;; (add-hook 'prog-mode-hook #'hs-minor-mode)
+  ;; (global-set-key (kbd "C-c <right>") 'hs-show-block)
+  ;; (global-set-key (kbd "C-c <left>") 'hs-hide-block)
+  ;;
+  ;;
+  ;; ;; (add-hook 'js2-mode-hook
   ;;      (lambda ()
-    ;;      (add-hook 'before-save-hook 'prettier-js nil 'make-it-local)))
+  ;;      (add-hook 'before-save-hook 'prettier-js nil 'make-it-local)))
 
-(use-package projectile
-    :ensure t
-    :config
-    (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
-    (projectile-mode +1 ))
+  ;; Setup the JS as followis
+  ;; https://www.youtube.com/watch?v=0zuYCEzrchk
 
-;; Hybrid is slower than alein but use both native and git indexing
-(setq projectile-indexing-method 'hybrid)
-;; Alien is the Fastest, Where it checkes the Git for File Indexing
-(setq projectile-indexing-method 'alien)
+  ;;(setq lsp-keymap-prefix "s-l")
 
-(setq projectile-git-submodule-command nil);; This is to support the Git Indexing, Without this it will fail
+  ;;(use-package lsp-mode
+  ;;  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+  ;;         (js2-mode . lsp)
+  ;;         ;; if you want which-key integration
+  ;;         (lsp-mode . lsp-enable-which-key-integration))
+  ;;  :commands lsp)
+
+  ;; optionally
+  ;;(use-package lsp-ui :commands lsp-ui-mode)
+  ;; if you are helm user
+  ;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+  ;; if you are ivy user
+  ;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+  ;;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+  ;; optionally if you want to use debugger
+  ;;(use-package dap-mode)
+  ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+
+
+;; (use-package js2-mode
+;;   :init (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+;;
+;; (use-package flycheck
+;;   :init
+;;   (add-hook 'prog-mode-hook 'flycheck-mode)    ;;  global-flycheck-mode
+;;   (setq flycheck-display-errors-delay .4))
+;;
+;; (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+;;       flycheck-idle-change-delay 0.8)
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+;;
+;;
+;; (use-package company
+;;   :init (add-hook 'prog-mode-hook 'company-mode)
+;;   :config (setq company-tooltip-align-annotations t) ;; aligns annotation to the right hand side
+;;   (setq company-minimum-prefix-length 1))
+;;
+;; (use-package lsp-mode
+;;   :commands lsp
+;;   :hook
+;;   (js2-mode . #'lsp))
+;;
+;; (use-package lsp-ui
+;;   :commands lsp-ui-mode
+;;   :hook
+;;   (lsp-mode . lsp-ui-mode))
+;;
+;; (use-package company-lsp
+;;   :commands company-lsp)
+;;
+;; (push 'company-lsp company-backends)
+
+;;  (use-package projectile
+  ;;    :ensure t
+  ;;    :config
+  ;;    (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  ;;    (projectile-mode +1 ))
+
+  (use-package projectile
+    :init (progn
+            (setq projectile-enable-caching t)
+            (setq projectile-indexing-method 'hybrid)
+            (setq projectile-globally-ignored-directories '("node_modules" "node_modules/" "dist" "dist/" "coverage" ))
+            (setq projectile-ignored-directories '("_output" "node_modules" "node_modules/" "pkg" "dist" "dist/" "dist/js" "coverage" ""))
+            (setq projectile-ignored-files '(".DS_Store" ".gitmodules" "package-lock.json" "yarn.lock" ".svg" "#" "~" "yarn-error.log" ".log" "*log" "yarn*"))
+            )
+    :bind (
+           ("C-x p" . projectile-command-map))
+    :config (projectile-mode 1))
+
+
+  ;; Hybrid is slower than alein but use both native and git indexing
+;;  (setq projectile-indexing-method 'hybrid)
+  ;; Alien is the Fastest, Where it checkes the Git for File Indexing
+  ;;(setq projectile-indexing-method 'alien)
+  (setq projectile-git-submodule-command nil);; This is to support the Git Indexing, Without this it will fail
 
 (use-package magit
   :ensure t
@@ -412,3 +496,58 @@
                 (lambda()
                   (interactive)
                   (rotate-windows -1)))
+
+(use-package js2-mode
+  :ensure t
+  :mode ("\\.js" . js2-mode)
+  :init
+  (setq js2-global-externs '("it" "afterEach" "beforeEach" "before" "after" "describe" "require" "module"))
+  :config
+  (setq-default js2-basic-offset 2)
+  (setq js-indent-level 2))
+
+
+(use-package json-mode
+  :ensure t
+  :mode ("\\.json" . json-mode))
+
+
+(use-package js2-refactor
+  :ensure t
+  :requires js2-mode)
+
+(use-package prettier-js
+  :ensure t
+  :requires js2-mode
+  :config
+  (setq prettier-js-args '("--trailing-comma" "all"
+                           "--bracket-spacing" "false"
+                           "--print-width" "200")))
+
+
+
+;;======================== LSP Mode ====================
+
+(use-package lsp-mode
+  :ensure t
+  :init (setq lsp-keymap-prefix "C-;")
+  :hook ((js2-mode . lsp)
+         (yaml-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp lsp-deferred
+  :ensure-system-package
+  ((typescript-language-server . "npm install -g typescript-language-server")
+   (javascript-typescript-langserver . "npm install -g javascript-typescript-langserver")
+   (yaml-language-server . "npm install -g yaml-language-server")
+   (tsc . "npm install -g typescript")))
+
+(use-package lsp-ui
+  :ensure t
+  ;; flycheck integration & higher level UI modules
+  :commands lsp-ui-mode)
+
+(use-package company-lsp
+  :ensure t
+  ;; company-mode completion
+  :commands company-lsp
+  :config (push 'company-lsp company-backends))
--- init.el
+++ init.el
@@ -3,10 +3,22 @@
 (when (version< emacs-version "26.0")
   (warn "This configuration needs Emacs trunk, but this is %s!" emacs-version))
 
+; try to improve slow performance on windows.
+(setq w32-get-true-file-attributes nil)
+
+
 ;; Increase the garbage collection threshold to 500 MB to ease startup
-(setq gc-cons-threshold (* 500 1024 1024))
+(setq gc-cons-threshold (* 500 1024 1024)
+      gc-cons-percentage 0.6
+      ;; gc-cons-threshold (* 1024 1024 1024) ;1G
+      jit-lock-stealth-time 0.1
+      jit-lock-chunk-size 100
+      jit-lock-defer-time 0.1)
 ;; Garbage collector - decrease threshold to 5 MB after startup
 (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+
+;; maybe improve performance on windows
+(setq w32-pipe-read-delay 0)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -35,7 +47,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (prettier-js js2-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu spaceline dashboard rainbow-delimiters hungry-delete hugry-delete switch-window avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update))))
+    (company-lsp lsp-ui lsp-mode company-tern use-package-ensure-system-package js2-refactor json-mode whole-line-or-region prettier-js js2-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu spaceline dashboard rainbow-delimiters hungry-delete hugry-delete switch-window avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -45,13 +57,6 @@
 
 
 
-(defun my-test-func ()
-  (interactive)
-(async-start
-   ;; What to do in the child process
-   (lambda ()
-     (message "This is a test")
-     (magit-status)
-   ;; What to do when it finishes
-   (lambda (result)
-     (message "Async process done, result should be 222: %s" result))))
+
+
+
