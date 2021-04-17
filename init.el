21bdc4931d89b21f0c94bf9f63a33d2df2ba96e2
Uditha Karunaratne <udithaindika@gmail.com>
2021-04-17 13:08:16 +1000

    Fix GC memory increse issue, and run EXWM if no other WM

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
       jit-lock-chunk-size 100
       jit-lock-defer-time 0.1)
 ;; Garbage collector - decrease threshold to 5 MB after startup
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+(add-hook 'emacs-startup-hook (lambda ()(setq gc-cons-threshold 800000)))
 
 ;; maybe improve performance on windows
 (setq w32-pipe-read-delay 0)
@@ -38,7 +39,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-quickhelp company-try-hard company-web company-statistics elpy dap-mode exwm elm-mode aggressive-indent smartparens all-the-icons company-lsp lsp-ui lsp-mode company-tern use-package-ensure-system-package js2-refactor json-mode whole-line-or-region prettier-js js2-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu spaceline dashboard rainbow-delimiters hungry-delete hugry-delete switch-window avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update)))
+   '(memory-usage company-quickhelp company-try-hard company-web company-statistics elpy dap-mode exwm elm-mode aggressive-indent smartparens all-the-icons company-lsp lsp-ui lsp-mode company-tern use-package-ensure-system-package js2-refactor json-mode whole-line-or-region prettier-js js2-mode expand-region mark-multiple swiper popup-kill-ring symon dmenu spaceline dashboard rainbow-delimiters hungry-delete hugry-delete switch-window avy smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package paradox gotest go-projectile gnu-elpa-keyring-update flycheck exec-path-from-shell diminish company-go benchmark-init auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
