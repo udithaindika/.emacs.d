cf8649498b6b3634afe5e123ca5e6a1ca4c5df3c
Uditha Karunaratne <udithaindika@gmail.com>
2021-09-21 19:50:40 +1000

    Mac updates

--- init.el
+++ init.el
@@ -45,5 +45,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#abb2bf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 98 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#abb2bf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-bold :height 120 :width normal :foundry "ADBO" :family "Source Code Pro")))))
 (put 'list-timers 'disabled nil)
